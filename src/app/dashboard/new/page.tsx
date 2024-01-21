608d112ae2d8bb4ad496f3020e99d7e704eb7ba0
Victor-Lis <victorlisbronzo1@gmail.com>
2024-01-20 21:24:35 -0300

    v1.19
    - Recebendo clientes do user;
    - Verificando quantidade clientes;
    - Se a quantidade de clientes for igual 0, não é possível completar o cadastro de um chamado e é possível clicar na aba "adicionar cliente"
    - Se a quantidade de clientes for maior que 0, a área da tag select aparecerá e os clientes serão mapeados e adicionados na tag select como options.

--- src/app/dashboard/new/page.tsx
+++ src/app/dashboard/new/page.tsx
@@ -1,56 +1,86 @@
-import { authOptions } from "@/lib/auth";
 import { Container } from "@/styled-components/container";
-import { getServerSession } from "next-auth";
-import Link from "next/link";
-import { redirect } from "next/navigation";
+import Link from 'next/link'
+
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/lib/auth'
+import { redirect } from 'next/navigation'
+import prismaClient from '@/lib/prisma'
 
 export default async function NewTicket() {
-
-  const session = await getServerSession(authOptions)
+  const session = await getServerSession(authOptions);
 
   if (!session || !session.user) {
     redirect("/")
   }
 
+  const customers = await prismaClient.customer.findMany({
+    where: {
+      userId: session.user.id
+    }
+  })
+
   return (
     <Container>
       <main className="mt-9 mb-2">
         <div className="flex items-center gap-3">
-          <Link
-            href="/dashboard"
-            className="text-white px-4 py-1 rounded bg-gray-900"
-          >
+          <Link href="/dashboard" className="text-white px-4 py-1 rounded bg-gray-900">
             Voltar
           </Link>
-          <h1 className="text-3xl font-bold"> </h1>
+          <h1 className="text-3xl font-bold">Novo chamados</h1>
         </div>
 
         <form className="flex flex-col mt-6">
-            <label className="mb-1 font-medium text-lg">Nome do chamado</label>
-            <input
-                className="w-full border-2 rounded-md px-2 mb-2 h-11"
-                type="text"
-                required
-            />
-            <label className="mb-1 font-medium text-lg">Descreva o problema</label>
-            <textarea
-                className="w-full border-2 rounded-md px-2 mb-2 h-24 resize-y"
-                required
-            ></textarea>
+          <label className="mb-1 font-medium text-lg">Nome do chamado</label>
+          <input
+            className="w-full border-2 rounded-md px-2 mb-2 h-11"
+            type="text"
+            placeholder="Digite o nome do chamado"
+            required
+          />
 
-            <label className="mb-1 font-medium text-lg">Adicione o cliente</label>
-            <select
-                className="w-full border-2 rounded-md px-2 mb-2 h-11 bg-white"
-                required
-            >
+          <label className="mb-1 font-medium text-lg">Descreva o problema</label>
+          <textarea
+            className="w-full border-2 rounded-md px-2 mb-2 h-24 resize-none"
+            placeholder="Descreva o problema..."
+            required
+          ></textarea>
 
-              <option value="cliente1">Cliente 1</option>
+          {customers.length !== 0 && (
+            <>
+              <label className="mb-1 font-medium text-lg">Selecione o cliente</label>
+              <select
+                className="w-full border-2 rounded-md px-2 mb-2 h-11 resize-none bg-white"
+              >
+                {customers.map(customer => (
+                  <option
+                    key={customer.id}
+                    value={customer.id}
+                  >
+                    {customer.name}
+                  </option>
+                ))}
+              </select>
+            </>
+          )}
 
-            </select>
+          {customers.length === 0 && (
+            <Link href="/dashboard/customer/new">
+              Você ainda não tem nenhum cliente, <span className="text-blue-500 font-medium">Cadastrar cliente</span>
+            </Link>
+          )}
+
+
+          <button
+            type="submit"
+            className="bg-blue-500 text-white font-bold px-2 h-11 rounded-md my-4 disabled:bg-gray-400 disabled:cursor-not-allowed"
+            disabled={customers.length === 0}
+          >
+            Cadastrar
+          </button>
 
         </form>
 
       </main>
     </Container>
-  );
+  )
 }
