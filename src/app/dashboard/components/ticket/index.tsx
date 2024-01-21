1df23810bcb0360b5ce738480fa1cdf59e399f9d
Victor-Lis <victorlisbronzo1@gmail.com>
2024-01-21 11:53:19 -0300

    v1.24
    - Adicionando resposta visual para o usuário enquanto ocorre o update do status do chamado(ticket).

--- src/app/dashboard/components/ticket/index.tsx
+++ src/app/dashboard/components/ticket/index.tsx
@@ -1,35 +1,48 @@
-"use client"
+"use client";
 import { FiCheckSquare, FiFile } from "react-icons/fi";
 import { TicketProps } from "@/@types/ticket";
 import { CustomerProps } from "@/@types/customer";
-import { api } from '@/lib/api'
+import { api } from "@/lib/api";
 import { useRouter } from "next/navigation";
-interface TicketItemProps{
-  ticket: TicketProps,
-  customer: CustomerProps | null,
+import { useState } from "react";
+interface TicketItemProps {
+  ticket: TicketProps;
+  customer: CustomerProps | null;
 }
 
-export function TicketItem({ticket, customer}: TicketItemProps) {
+export function TicketItem({ ticket, customer }: TicketItemProps) {
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
 
-  const router = useRouter()
+  async function handleChangeStatus() {
+    if (!loading) {
+      setLoading(true);
+      try {
+        const response = await api.patch("/api/ticket", {
+          id: ticket.id,
+        });
 
-  async function handleChangeStatus(){
-    try{
-      const response = await api.patch("/api/ticket", {
-        id: ticket.id
-      })
-
-      router.refresh()
-      console.log(response.data)
-    }catch(err){
-      console.log(err)
+        router.refresh();
+        console.log(response.data);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setLoading(false);
+      }
     }
   }
 
   return (
-    <tr className="border-b-2 border-b-slate-200 h-16 last:border-b-0 bg-slate-100 hover:bg-gray-200 duration-300">
+    <tr className=
+    {!loading 
+      ? "border-b-2 border-b-slate-200 h-16 last:border-b-0 bg-slate-100 hover:bg-gray-200 duration-300"
+      : "border-b-2 border-b-slate-200 h-16 last:border-b-0 bg-gray-200"
+      }
+    >
       <td className="text-left pl-1">{ticket?.name}</td>
-      <td className="text-left hidden md:table-cell">{ticket.created_at?.toLocaleDateString("pt-br")}</td>
+      <td className="text-left hidden md:table-cell">
+        {ticket.created_at?.toLocaleDateString("pt-br")}
+      </td>
       <td className="text-left">
         <span className="bg-green-500 px-2 py-1 rounded">{ticket.status}</span>
       </td>
